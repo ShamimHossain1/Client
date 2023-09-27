e3f12132eb4aaed85df5aea22acbb7a42f9158df
ShamimHossain1 <beastb697@gmail.com>
2023-09-27 21:17:20 +0600

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then((data) => {
+        setUsers(data)
+      })
+  })
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user)
+    fetch('http://localhost:5000/users',{
+      method:'POST', 
+      headers:{
+        "Content-Type": "application/json",
+        },
+        body : JSON.stringify(user)
+    })
+    .then (response=> response.json ())
+    .then ((data)=> 
+    {
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+      form.reset()
+    }
+    )
+
+  }
 
   return (
     <>
+      <h1>Hello World</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="text" name="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <h1>Total Data {users.length}</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {
+          users.map(user => <p key={user.id}>
+            {user.id} : {user.name} : {user.email}
+          </p>)
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }
